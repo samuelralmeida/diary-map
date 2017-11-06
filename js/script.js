fe2f790b1a48c704299ed7272690479244006f56
Samuel Almeida <samuel.ralmeida@gmail.com>
2017-11-06 09:10:02 -0200

    feat: implemented marker info window
    body: script code was organized too

--- js/script.js
+++ js/script.js
@@ -1,27 +1,24 @@
 var googleKey = config.GOOGLE_KEY;
-
-// Função para carregamento assíncrono
-window.addEventListener('load',function(){
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?libraries=places,geometry,drawing&key='+googleKey+'&v=3&callback=initMap';
-    document.body.appendChild(script);
-});
-
 var map;
-// Create placemarkers array to use in multiple functions to have control
-// over the number of places that show.
-var placeMarker = [];
-// função para carregar o mapa
-// função chamada pelo script abaixo para carregar api
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    // criar uma instancia de novo mapa
-    // define pela id onde vai aparecer no html
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -19.925382, lng: -43.942943},
-        zoom: 15
-    });
+// model of wishes
+var Wish = function(data) {
+
+};
+
+// model of achievements
+var Achievement = function(data) {
+
+};
+
+var ChosenPlace = function(data) {
+    this.chosenPlace = ko.observable(data.chosenPlace);
+    // recebe uma lista de objetos
+    this.wikipediaLinks = ko.observableArray(data.wikipediaLinks);
+}
+
+var ViewModel = function() {
+    var self = this;
+
 
     // Create a searchbox in order to execute a places search
     var searchBox = new google.maps.places.SearchBox(document.getElementById('places-search'));
@@ -34,21 +31,17 @@ function initMap() {
     // This function fires when the user selects a searchbox picklist item.
     // It will do a nearby search using the selected query string or place.
     function searchBoxPlaces(searchBox) {
-        //**
         var places = searchBox.getPlaces();
         if (places.length == 0) {
             window.alert('We did not find any places matching that search!');
         } else {
             // For each place, get the icon, name and location.
-            //**
             createMarkersForPlaces(places);
-            //console.log('places', places)
         }
     }
     // This function firest when the user select "go" on the places search.
     // It will do a nearby search using the entered query string or place.
     function textSearchPlaces() {
-        //**
         var bounds = map.getBounds();
         var placesService = new google.maps.places.PlacesService(map);
         placesService.textSearch({
@@ -56,13 +49,11 @@ function initMap() {
             bounds: bounds
         }, function(results, status) {
             if (status === google.maps.places.PlacesServiceStatus.OK) {
-                //**
                 createMarkersForPlaces(results);
-                //console.log('results', results)
             }
         });
     }
-    // This function creates markers for each place found in either places search.
+
     function createMarkersForPlaces(places) {
         var bounds = new google.maps.LatLngBounds();
         for (var i = 0; i < places.length; i++) {
@@ -79,6 +70,7 @@ function initMap() {
                 map: map,
                 title: place.name,
                 position: place.geometry.location,
+                animation: google.maps.Animation.DROP,
                 id: place.place_id
             });
             // Create a single infowindow to be used with the place details information
@@ -89,56 +81,78 @@ function initMap() {
                 if (placeInfoWindow.marker == this) {
                     console.log("This infowindow already is on this marker!");
                 } else {
-                    getPlacesDetails(this, placeInfoWindow);
+                  getPlacesDetails(this, placeInfoWindow);
                 }
             });
-            placeMarker = [];
-            placeMarker.push(marker);
+            //placeMarkers.push(marker);
             if (place.geometry.viewport) {
                 // Only geocodes have viewport.
                 bounds.union(place.geometry.viewport);
-            } else {
+              } else {
                 bounds.extend(place.geometry.location);
+              }
+      }
+      map.setZoom(10);
+      map.fitBounds(bounds);
+    }
+
+    function getPlacesDetails(marker, infowindow) {
+        var service = new google.maps.places.PlacesService(map);
+        service.getDetails({placeId: marker.id}, function(place, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                // Set the marker property on this infowindow so it isn't created again.
+                infowindow.marker = marker;
+                var innerHTML = '<div>';
+                if (place.name) {
+                    innerHTML += '<strong>' + place.name + '</strong>';
+                }
+                if (place.formatted_address) {
+                    innerHTML += '<br>' + place.formatted_address;
+                }
+                if (place.formatted_phone_number) {
+                    innerHTML += '<br>' + place.formatted_phone_number;
+                }
+                if (place.opening_hours) {
+                    innerHTML += '<br><br><strong>Hours:</strong><br>' +
+                    place.opening_hours.weekday_text[0] + '<br>' +
+                    place.opening_hours.weekday_text[1] + '<br>' +
+                    place.opening_hours.weekday_text[2] + '<br>' +
+                    place.opening_hours.weekday_text[3] + '<br>' +
+                    place.opening_hours.weekday_text[4] + '<br>' +
+                    place.opening_hours.weekday_text[5] + '<br>' +
+                    place.opening_hours.weekday_text[6];
+                }
+                if (place.photos) {
+                    innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
+                        {maxHeight: 100, maxWidth: 200}) + '">';
+                }
+                innerHTML += '</div>';
+                infowindow.setContent(innerHTML);
+                infowindow.open(map, marker);
+                // Make sure the marker property is cleared if the infowindow is closed.
+                infowindow.addListener('closeclick', function() {
+                    infowindow.marker = null;
+                });
             }
-        }
-        console.log(placeMarker)
-        map.fitBounds(bounds);
-    }
-}
-
-var Wish = function(data) {
-
-}
-
-var ViewModel = function() {
-    var self = this;
-    this.wishOrRealized = ko.observable();
-    this.inputWish = ko.observable(false);
-    this.inputAnchievement = ko.observable(false);
-
-    this.wishOrRealized.subscribe(function(newValue){
-        this.showForm(newValue);
-    }, this);
-
-    this.showForm = function(value) {
-        if (value === 'wish') {
-            this.inputAnchievement(false);
-            this.inputWish(true);
-            this.scrollView('step');
-        } else if (value === 'realized'){
-            this.inputAnchievement(true);
-            this.inputWish(false);
-            this.scrollView('step');
-        } else {
-            this.inputAnchievement(false);
-            this.inputWish(false);
-        }
+        });
     }
 
-    this.scrollView = function(value) {
-        document.getElementById(value).scrollIntoView();
-    }
+};
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -19.925382, lng: -43.942943},
+        zoom: 4
+    });
+
+    ko.applyBindings(new ViewModel());
 }
 
-ko.applyBindings(new ViewModel)
+window.addEventListener('load',function(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = "https://maps.googleapis.com/maps/api/js?key="+googleKey+"&libraries=geometry,places&callback=initMap";
+    script.setAttribute('defer','');
+    script.setAttribute('async','');
+    document.body.appendChild(script);
+});
